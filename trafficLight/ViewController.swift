d5f14004b8d4daf0fa92f3691761dcbb6f5d1d85
Tanjerus <Merserus@gmail.com>
2021-12-08 20:10:00 +0300

    ready interface

--- trafficLight/ViewController.swift
+++ trafficLight/ViewController.swift
@@ -8,12 +8,21 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    @IBOutlet var redLightView: UIView!
+    @IBOutlet var yelowLightView: UIView!
+    @IBOutlet var greenLightView: UIView!
+    @IBOutlet var startButton: UIButton!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        redLightView.layer.cornerRadius = 75
+        yelowLightView.layer.cornerRadius = 75
+        greenLightView.layer.cornerRadius = 75
     }
 
-
+    @IBAction func startPressed() {
+    }
+    
 }
 
